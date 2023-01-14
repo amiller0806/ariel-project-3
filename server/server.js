2f8b496b809320d8d9f9a13a1fe81ff47742e4b0
amiller0806 <ariel.miller555@gmail.com>
2023-01-14 16:36:44 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -76,3 +76,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
+
+
+// Updated Package.json, and 
