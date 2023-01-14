6abaa7c3a1658377020433af79ca8a9f9e1b5fb5
amiller0806 <ariel.miller555@gmail.com>
2023-01-14 15:54:20 -0500

    fixing deploy issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deploy-skincare",
+  process.env.MONGODB_URI || "mongodb://localhost:3000/deploy-skincare",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
