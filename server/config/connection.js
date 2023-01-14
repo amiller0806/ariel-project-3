76b27257124664a136dd78d0e2d5b72c7aa8d8d1
amiller0806 <ariel.miller555@gmail.com>
2023-01-14 15:56:53 -0500

    fixing deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:3000/deploy-skincare",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/deploy-skincare",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
